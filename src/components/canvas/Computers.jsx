f69b92a0d3dfc9a0d96cdc052c355dbe36ff85ce
Voxusss <antoine4013@gmail.com>
2023-03-14 16:24:38 +0100

    shamelessly following tutorial pt.2

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import {Suspense, useEffect, useState} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('./killer_beats_mixstation/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={1} groundColor='white' />
+      <spotLight 
+        position={[10,10,10]}
+        angle={1}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive 
+      object={computer.scene}
+      scale={isMobile ? 0.2 : 0.25}
+      position={[0, -3, 0]}
+      rotation={[-0.01, 1, 0]}
+      />
+    </mesh>
   )
 }
-
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    };
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+  }, []);
+  return(
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{position: [20,3,5], fov:25}}
+      gl={{preserveDrawingBuffer:true}}
+    > 
+      <Suspense fallback={<CanvasLoader/>}>
+        <OrbitControls 
+          enableZoom={false}
+          minPolarAngle={Math.PI/2}
+          maxPolarAngle={Math.PI/2}
+        />
+        <Computers isMobile={isMobile}/>
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+export default ComputersCanvas
